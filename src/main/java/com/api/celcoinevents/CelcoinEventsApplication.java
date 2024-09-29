6bcf2ef5aeb2666d6897f22e71cb18797aefa357
nrg-drm <su.re44e@gmail.com>
2024-09-29 15:40:56 -0300

    feat: add code base

---
+++ src/main/java/com/api/celcoinevents/CelcoinEventsApplication.java
@@ -0,0 +1,13 @@
+package com.api.celcoinevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelcoinEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelcoinEventsApplication.class, args);
+	}
+
+}
