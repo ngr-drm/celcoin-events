20c2706df61ff2530a2af0e585291912fcd89ea5
nrg-drm <su.re44e@gmail.com>
2024-09-29 15:45:31 -0300

    test(celcoin): add unit test for sending events

---
+++ src/test/java/com/api/celcoinevents/CelcoinEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.celcoinevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelcoinEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
